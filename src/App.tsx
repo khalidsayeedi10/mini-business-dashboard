0b9f9ea06516e0df36906de8a87022655875d170
Khalid <khalidsayeedi10@gmail.com>
2026-09-07 23:10:42 +0430

    Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 
 export default function App() {
 	return (
 		<div className="app">
+			<Header />
 			<Sidebar />
 		</div>
 	);
